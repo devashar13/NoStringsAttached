f1a42605da39ce9bb9501b9a94f78fd307c1bad7
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2021-04-10 12:24:28 +0530

    feat: started uploadSong funcs

--- src/contracts/Nostringsattached.sol
+++ src/contracts/Nostringsattached.sol
@@ -1,9 +1,15 @@
 pragma solidity ^0.5.0;
 string public name = "Nostringsattached";
-struct Music{
+mapping(uint => Song) public songs;
+struct Song{
     uint id;
     string hash;
     string description;
     uint tipAmount; 
     address payable author;
 }
+  function uploadSong(string memory _songHash, string memory _description) public {
+    require(bytes(_description).length>0);
+    require(bytes(_songHash).length>0);
+    require(msg.sender!=address(0x0));
+  }
