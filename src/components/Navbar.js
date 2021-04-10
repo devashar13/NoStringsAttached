a5d0c46059880ff7e107ede1fcd813aadc96759e
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2021-04-10 13:14:02 +0530

    displaying navbar frontend fml

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Component } from "react";
+import Identicon from "identicon.js";
+import photo from "../photo.png";
 
-const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
 
-export default Navbar
+const Navbar = ({ account }) => {
+  return (
+    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a
+        className="navbar-brand col-sm-3 col-md-2 mr-0"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        NoStringsAttached
+      </a>
+      <ul className="navbar-nav px-3">
+        <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+          <small className="text-secondary">
+            <small id="account">{account}</small>
+          </small>
+          
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
