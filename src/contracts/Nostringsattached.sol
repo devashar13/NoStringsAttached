b03ea911d36810040112e4d4f6ce3a1e8845e5e3
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2021-04-10 12:27:36 +0530

    fix:added events

--- src/contracts/Nostringsattached.sol
+++ src/contracts/Nostringsattached.sol
@@ -1,6 +1,14 @@
 pragma solidity ^0.5.0;
 string public name = "Nostringsattached";
 mapping(uint => Song) public songs;
+uint public songCount = 0;
+ event SongCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
 struct Song{
     uint id;
     string hash;
@@ -12,4 +20,6 @@ struct Song{
     require(bytes(_description).length>0);
     require(bytes(_songHash).length>0);
     require(msg.sender!=address(0x0));
+    songCount++; 
+    songs[songCount] = Song(songCount,_songHash,_description,0,msg.sender);
   }
